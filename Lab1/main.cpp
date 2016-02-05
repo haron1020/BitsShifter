a1b858e5160da456a1f30965f1dc4bfaf8257f22
Andrey Nazarov <haron1020@yandex.ru>
2016-02-05 15:48:23 +0300

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Andrey Nazarov on 05/02/16.
+//  Copyright © 2016 Andrey Nazarov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
